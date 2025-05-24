dbe47f04d42e8092d55fee1ede779980ec8ec6f2
Nate <nathanael.paul.lewis@gmail.com>
2025-05-24 15:58:35 -0500

    Updated files on 2025-05-24

--- config/fish.sh
+++ config/fish.sh
@@ -2,5 +2,5 @@
 
 package="Fish Shell"
 echo "Copying $package files... "
-cp "$HOME/projects/dev-env/files/fish/" "$HOME/.config/fish/" -rf
+cp "$HOME/projects/dev-env/files/fish/" "$HOME/.config/" -rf
 echo "Copying $package files Complete."
--- config/neovim.sh
+++ config/neovim.sh
@@ -2,5 +2,5 @@
 
 package="Neovim"
 echo "Copying $package files... "
-cp "$HOME/projects/dev-env/files/nvim/" "$HOME/.config/nvim/" -rf
+cp "$HOME/projects/dev-env/files/nvim/" "$HOME/.config/." -rf
 echo "Copying $package files Complete."
