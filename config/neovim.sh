d215239fd1f8bfaa0749adc6c19d84cd9fb7656d
Nate <nathanael.paul.lewis@gmail.com>
2025-06-25 22:54:13 -0500

    Updated files on 2025-06-25

--- config/neovim.sh
+++ config/neovim.sh
@@ -4,7 +4,6 @@ package="Neovim"
 echo "Copying $package files... "
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
-cp -f "$HOME/projects/dev-env/files/nvim/lua/plugins/avante.lua" "$HOME/.config/nvim/lua/plugins/."
 cp -f "$HOME/projects/dev-env/files/nvim/lua/plugins/colorscheme.lua" "$HOME/.config/nvim/lua/plugins/."
 cp -f "$HOME/projects/dev-env/files/nvim/lua/config/keymaps.lua" "$HOME/.config/nvim/lua/config/."
 cp -f "$HOME/projects/dev-env/files/nvim/lua/config/options.lua" "$HOME/.config/nvim/lua/config/."
